4cdc971485925291cc2ddcfe78e64da588a0af53
Maksim Aisin <maksim.aisin@mail.ru>
2022-01-02 23:26:42 +0300

    feat: set up array references, num<20 feature

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    let one = [
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "nineteen",
+    ];
+    let ten = [
+        "twenty",
+        "thirty",
+        "fourty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "ninety",
+    ];
+    let hundred = "hundred";
+    let thouthand = "thousand";
+
+    let valueLength = number.length;
+
+    if (number < 20) {
+        return one[number];
+    }
+};
